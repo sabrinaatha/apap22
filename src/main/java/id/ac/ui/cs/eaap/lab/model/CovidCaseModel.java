22a7c18a7c54bcd0c42d918409f044fce83349b9
sabrinaatha <sabrinaathashania@gmail.com>
2024-10-21 22:16:25 +0700

    fix #7

--- src/main/java/id/ac/ui/cs/eaap/lab/model/CovidCaseModel.java
+++ src/main/java/id/ac/ui/cs/eaap/lab/model/CovidCaseModel.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Data
@@ -32,5 +37,12 @@ public class CovidCaseModel implements Serializable {
     private String peran;
     @Column(name = "fakultas")
     private String fakultas;
+
+    public long getHariSetelahGejalaPertama() {
+        java.util.Date date = new java.util.Date();
+        long value = TimeUnit.DAYS.convert(date.getTime() - tanggalGejalaPertama.getTime(), TimeUnit.MILLISECONDS);
+
+        return value;
+    }
 
 }
